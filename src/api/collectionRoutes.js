13857eefc467260b501143610a747cf0b752851a
David Esposito <david@espo.email>
2020-10-29 06:00:07 -0400

    Add /ns namespace slug to data routes

--- src/api/collectionRoutes.js
+++ src/api/collectionRoutes.js
@@ -1,7 +1,13 @@
 const { count, get, list, put, remove, truncate } = require('../db/index.js');
 
+const VERSION_PREFIX = `/v1`;
+const NAMESPACE_PREFIX = `/ns`;
+
+const nsRoute = (relativePath) =>
+  `${VERSION_PREFIX}/${NAMESPACE_PREFIX}${relativePath}`;
+
 function init(app) {
-  app.get('/v1/:namespace', async (req, res) => {
+  app.get(nsRoute('/:namespace'), async (req, res) => {
     res.send(
       await list(
         req.params.namespace,
@@ -11,31 +17,31 @@ function init(app) {
     );
   });
 
-  app.get('/v1/:namespace/__count', async (req, res) => {
+  app.get(nsRoute('/:namespace/__count'), async (req, res) => {
     res.send(await count(req.params.namespace));
   });
 
-  app.get('/v1/:namespace/__info', (req, res) => {
+  app.get(nsRoute('/:namespace/__info'), (req, res) => {
     // TODO:
     res.send(INFO);
   });
 
-  app.get('/v1/:namespace/:key', async (req, res) => {
+  app.get(nsRoute('/:namespace/:key'), async (req, res) => {
     res.send(await get(req.params.namespace, req.params.key));
   });
 
-  app.post('/v1/:namespace', async (req, res) => {
+  app.post(nsRoute('/:namespace'), async (req, res) => {
     const {
       body: { key, value },
     } = req;
     res.send(await put(req.params.namespace, key, value));
   });
 
-  app.delete('/v1/:namespace', async (req, res) => {
+  app.delete(nsRoute('/:namespace'), async (req, res) => {
     res.send(await truncate(req.params.namespace));
   });
 
-  app.delete('/v1/:namespace/:key', async (req, res) => {
+  app.delete(nsRoute('/:namespace/:key'), async (req, res) => {
     res.send(await remove(req.params.namespace, req.params.key));
   });
 }
